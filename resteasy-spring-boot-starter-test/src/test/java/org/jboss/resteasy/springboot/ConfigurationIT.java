b97401ec2822924ebc83baf402dbc511a29a6881
阿男 <l.weinan@gmail.com>
2019-12-14 17:04:35 +0800

    cleanup unused imports

--- resteasy-spring-boot-starter-test/src/test/java/org/jboss/resteasy/springboot/ConfigurationIT.java
+++ resteasy-spring-boot-starter-test/src/test/java/org/jboss/resteasy/springboot/ConfigurationIT.java
@@ -1,6 +1,5 @@
 package org.jboss.resteasy.springboot;
 
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.WebApplicationType;
