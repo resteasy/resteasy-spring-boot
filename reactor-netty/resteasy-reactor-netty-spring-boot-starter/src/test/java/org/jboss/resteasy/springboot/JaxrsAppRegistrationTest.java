ab872c40b1fb293ea600ef2f6ad77dede9192591
Leslie Fokou <lalifk@gmail.com>
2022-07-13 22:52:45 -0500

    Typos and formatting fixes

--- reactor-netty/resteasy-reactor-netty-spring-boot-starter/src/test/java/org/jboss/resteasy/springboot/JaxrsAppRegistrationTest.java
+++ reactor-netty/resteasy-reactor-netty-spring-boot-starter/src/test/java/org/jboss/resteasy/springboot/JaxrsAppRegistrationTest.java
@@ -22,143 +22,126 @@ import org.testng.annotations.Test;
 import jakarta.ws.rs.core.Application;
 
 public class JaxrsAppRegistrationTest extends AbstractTestNGSpringContextTests {
-	
+
 	private final String SPRING_CONTEXT_FILE_LOCATION = "classpath:test-config.xml";
 
-	private enum JaxrsAppClassesRegistration {BEANS, PROPERTY, SCANNING, AUTO}
-	
-	private final List<Class<? extends Application>> expectedAppsAll = Stream
-			.of(
-			    TestApplication1.class, 
-			    TestApplication2.class,
-			    TestApplication3.class, 
-			    TestApplication4.class, 
-			    TestApplication5.class)
-			.collect(Collectors.toList());		
-	
-	
-    @Test
-    public void nullTest() {
+	private enum JaxrsAppClassesRegistration {
+		BEANS, PROPERTY, SCANNING, AUTO
+	}
+
+	private final List<Class<? extends Application>> expectedAppsAll = Stream.of(TestApplication1.class,
+			TestApplication2.class, TestApplication3.class, TestApplication4.class, TestApplication5.class)
+			.collect(Collectors.toList());
+
+	@Test
+	public void nullTest() {
 		verifyExpectedApps(expectedAppsAll, new CustomPropertySource());
 	}
-	
-	@Test
-    public void autoTest() {
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.AUTO.name());
-		verifyExpectedApps(expectedAppsAll, customPropertySource);
-	}
-	
-	@Test
-    public void scanningTest() {
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.SCANNING.name());
-		verifyExpectedApps(expectedAppsAll, customPropertySource);
-	}
-	
-	@Test
-    public void autoClassesSetTest() {
 
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.AUTO.name())
-                .setAppClasses(TestApplication3.class.getTypeName());
-		
-		final List<Class<? extends Application>> expectedApps = Stream
-				.of(TestApplication3.class)
-				.collect(Collectors.toList());		
-		
+	@Test
+	public void autoTest() {
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.AUTO.name());
+		verifyExpectedApps(expectedAppsAll, customPropertySource);
+	}
+
+	@Test
+	public void scanningTest() {
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.SCANNING.name());
+		verifyExpectedApps(expectedAppsAll, customPropertySource);
+	}
+
+	@Test
+	public void autoClassesSetTest() {
+
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.AUTO.name())
+				.setAppClasses(TestApplication3.class.getTypeName());
+
+		final List<Class<? extends Application>> expectedApps = Stream.of(TestApplication3.class)
+				.collect(Collectors.toList());
+
 		verifyExpectedApps(expectedApps, customPropertySource);
 	}
-	
-	
+
 	@Test
-    public void beansTest() {
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.BEANS.name());
+	public void beansTest() {
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.BEANS.name());
 		verifyExpectedApps(new ArrayList<>(), customPropertySource);
 	}
-	
-	
-	@Test
-    public void propertyTest() {
 
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name())
-                .setAppClasses(TestApplication1.class.getTypeName() + "," + TestApplication2.class.getTypeName());
-		
-	    final List<Class<? extends Application>> expectedApps = Stream
-				.of(
-						TestApplication1.class,
-						TestApplication2.class)
-				.collect(Collectors.toList());		
-		
+	@Test
+	public void propertyTest() {
+
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name())
+				.setAppClasses(TestApplication1.class.getTypeName() + "," + TestApplication2.class.getTypeName());
+
+		final List<Class<? extends Application>> expectedApps = Stream
+				.of(TestApplication1.class, TestApplication2.class).collect(Collectors.toList());
+
 		verifyExpectedApps(expectedApps, customPropertySource);
 
 	}
-	
-	
+
 	@Test
-    public void propertyClassesNotSetTest() {
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name());
+	public void propertyClassesNotSetTest() {
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name());
 		verifyExpectedApps(expectedAppsAll, customPropertySource);
 
 	}
-	
-	@Test(expectedExceptions = {BeansException.class})
-    public void propertyInvalidClassTest() {
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name())
-                .setAppClasses("does.not.exist.Com");
+
+	@Test(expectedExceptions = { BeansException.class })
+	public void propertyInvalidClassTest() {
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name())
+				.setAppClasses("does.not.exist.Com");
 		verifyExpectedApps(null, customPropertySource);
 
 	}
-	
+
 	@Test(expectedExceptions = IllegalArgumentException.class)
-    public void invalidRegistrationTest() {
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition("turkey");
+	public void invalidRegistrationTest() {
+		final CustomPropertySource customPropertySource = new CustomPropertySource().setAppClassesDefinition("turkey");
 		verifyExpectedApps(null, customPropertySource);
 	}
-	
-	
+
 	@Test
-    public void legacyPropertyTest() {
-	    final CustomPropertySource customPropertySource = new CustomPropertySource()
-                .setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name())
-                .setAppClasses(TestApplication3.class.getTypeName() + "," + TestApplication5.class.getTypeName()); 
-		
-	    final List<Class<? extends Application>> expectedApps = Stream
-				.of(
-						TestApplication3.class,
-						TestApplication5.class)
-				.collect(Collectors.toList());		
-		
+	public void legacyPropertyTest() {
+		final CustomPropertySource customPropertySource = new CustomPropertySource()
+				.setAppClassesDefinition(JaxrsAppClassesRegistration.PROPERTY.name())
+				.setAppClasses(TestApplication3.class.getTypeName() + "," + TestApplication5.class.getTypeName());
+
+		final List<Class<? extends Application>> expectedApps = Stream
+				.of(TestApplication3.class, TestApplication5.class).collect(Collectors.toList());
+
 		verifyExpectedApps(expectedApps, customPropertySource);
-		
+
 	}
-	
-	private void verifyExpectedApps (List<Class<? extends Application>> expectedApps, CustomPropertySource customPropertySource) {
-		
-        final ApplicationContext appContext = TestUtils.configureAndCreateAppContext(SPRING_CONTEXT_FILE_LOCATION,
-                customPropertySource, Optional.empty());
-		
-	    final ResteasyBeanProcessorReactorNetty beanProcessor = appContext.getBean(ResteasyBeanProcessorReactorNetty.class);
-		
-		if(expectedApps == null) {
+
+	private void verifyExpectedApps(List<Class<? extends Application>> expectedApps,
+			CustomPropertySource customPropertySource) {
+
+		final ApplicationContext appContext = TestUtils.configureAndCreateAppContext(SPRING_CONTEXT_FILE_LOCATION,
+				customPropertySource, Optional.empty());
+
+		final ResteasyBeanProcessorReactorNetty beanProcessor = appContext
+				.getBean(ResteasyBeanProcessorReactorNetty.class);
+
+		if (expectedApps == null) {
 			Assert.assertEquals(beanProcessor.getApplications(), 0);
-		}
-		else {
+		} else {
 			Assert.assertNotNull(beanProcessor.getApplications());
 			expectedApps.forEach(app -> {
 				beanProcessor.getApplications().contains(app);
 			});
 		}
-		
-		((ConfigurableApplicationContext)appContext).close();
-        
+
+		((ConfigurableApplicationContext) appContext).close();
+
 	}
 
-
-	
 }
