85824486a5c992196464a39767429784af9a6b98
阿男 <l.weinan@gmail.com>
2024-11-12 03:29:00 +0800

    add `springdoc-openapi-starter-webmvc-ui`

--- servlet/resteasy-wildfly-spring-boot-sample-app/src/main/java/com/sample/app/RestApp.java
+++ servlet/resteasy-wildfly-spring-boot-sample-app/src/main/java/com/sample/app/RestApp.java
@@ -2,10 +2,10 @@ package com.sample.app;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-import org.eclipse.microprofile.openapi.annotations.info.Info;
+//import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+//import org.eclipse.microprofile.openapi.annotations.info.Info;
 
 @ApplicationPath("/rest")
-@OpenAPIDefinition(info = @Info(title = "RestApp", version = "1.0.0"))
+//@OpenAPIDefinition(info = @Info(title = "RestApp", version = "1.0.0"))
 public class RestApp extends Application {
 }
