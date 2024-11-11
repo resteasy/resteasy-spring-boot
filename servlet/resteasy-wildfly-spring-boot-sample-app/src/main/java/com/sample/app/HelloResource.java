5ae36772b9e98cd0771187ace795e002d4050397
阿男 <l.weinan@gmail.com>
2024-11-12 03:29:00 +0800

    add `springdoc-openapi-starter-webmvc-ui`

--- servlet/resteasy-wildfly-spring-boot-sample-app/src/main/java/com/sample/app/HelloResource.java
+++ servlet/resteasy-wildfly-spring-boot-sample-app/src/main/java/com/sample/app/HelloResource.java
@@ -1,6 +1,6 @@
 package com.sample.app;
 
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+//import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +14,7 @@ public class HelloResource {
     @Autowired
     EchoBean bean;
 
-    @APIResponse(description = "return hello world")
+//    @APIResponse(description = "return hello world")
     @GET
     public String get() {
         return bean.echo("Hello, world!");
